6b62b01546ca260f0aebf6e8de96c4b4b41c3d2b
ConnorNiblett1 <connor@yard.bot>
2023-12-19 11:20:27 -0600

    Corrected inaccuracy of height measurement.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 /*
   ESP 32
   Distance from sensor to mower blades is 50mm.
-  Max Sensor Height: 140mm.
+  Blade Max Height: 90mm.
+  Max Sensor Height: 150mm.
   Min Sensor Height: mm.
   
   Teensy 4.1
@@ -11,8 +12,8 @@
 
   FC BOARD
   FC Address: N/A
-  FC SDA PIN: IO33 / 9
-  FC SCL PIN: IO25 / 10
+  FC SDA PIN: GPIO33 
+  FC SCL PIN: GPIO25  
 
 */
 
@@ -22,9 +23,10 @@
 constexpr uint8_t RELAY_1 = GPIO_NUM_14; //Up
 constexpr uint8_t RELAY_2 = GPIO_NUM_27; //Down
 constexpr unsigned char sensorAddress = 80;
-constexpr  float marginOfError = 0.05;
-constexpr unsigned char bladeHeightDifference = 50;
+constexpr  float tolerance = 3.0; // mm
+constexpr unsigned char bladeHeightDifference = 60;
 constexpr unsigned char packetLength = 2;
+constexpr unsigned char calibrationFactor = 3;
 constexpr int default_SDA = GPIO_NUM_33;
 constexpr int default_SCL = GPIO_NUM_25;
 
@@ -34,38 +36,54 @@ public:
 
 
 
-void moveDeckToInches(float desired_height_in ) { // User can override if deck height sensor is on new port, assumes default port 8.
+void moveDeckUp() {
+  digitalWrite(RELAY_1, HIGH);
+  digitalWrite(RELAY_2, LOW);
+}
+
+void moveDeckDown(){
+  digitalWrite(RELAY_2, HIGH);
+  digitalWrite(RELAY_1, LOW);
+}
+
+void stopDeck(){
+  digitalWrite(RELAY_1, LOW);
+  digitalWrite(RELAY_2, LOW);
+}
+
+void moveDeckToInches(float desired_height_in ) { 
   
-    float current_height_mm = checkHeight() - bladeHeightDifference; // Need a conidtion for what to do if 4 bytes are not avaliable. Using a loop would be blocking? 
+     // Need a conidtion for what to do if 4 bytes are not avaliable. Using a loop would be blocking? 
     float desired_height_mm = desired_height_in * 25.4; //Converts from inches to mm.
-    
-    while(current_height_mm > (desired_height_mm + marginOfError)) {
-      //Down
-      Serial.println("Down Condition called");
-      digitalWrite(RELAY_2, HIGH);
-      digitalWrite(RELAY_1, LOW);
+    bool flag = true;
+    while(flag == true) {
+      
+      float current_height_mm = checkHeight() - bladeHeightDifference - calibrationFactor;
+      int error =  current_height_mm - desired_height_mm;
 
-      current_height_mm = checkHeight() - bladeHeightDifference;;
-    }
-    while(current_height_mm < (desired_height_mm - marginOfError)) {
-      //Up
-      Serial.println("Up condition called");
-      digitalWrite(RELAY_1, HIGH);
-      digitalWrite(RELAY_2, LOW);
+      if(error > tolerance) {
+        moveDeckDown();
+        Serial.println("Moving Down");
 
-       current_height_mm = checkHeight() - bladeHeightDifference;;
-    }
-    while(current_height_mm == desired_height_mm) {
-      Serial.println("Correct height reached");
-      return;
-    }
-   
-
-  
-  
-  
-
+      }
+      else if(error < tolerance) {
+        moveDeckUp();
+        Serial.println("Moving up");
+      }
+      else{
+        stopDeck();
+        Serial.println("Deck stopping");
+        flag = false;
+      }
+      
+      }
   }
+
+  
+  
+  
+
+  
 
   float checkHeight(){
      Wire.requestFrom(sensorAddress,  packetLength);
@@ -92,8 +110,7 @@ void setup () {
 
 void loop () {
    myDeck.moveDeckToInches(2.5);
-   //Serial.println(myDeck.checkHeight());
-
-    
+   Serial.println("Main Loop Running");
+   delay(500);
   }
 
